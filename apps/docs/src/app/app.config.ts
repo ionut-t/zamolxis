67f6ef003fbd436cabcd330e937281590584c7fa
Ionut Traistaru <ionut.m.traistaru@gmail.com>
2024-02-05 15:42:55 +0000

    add angular material and styles for the docs app

--- apps/docs/src/app/app.config.ts
+++ apps/docs/src/app/app.config.ts
@@ -2,7 +2,8 @@ import { ApplicationConfig } from '@angular/core';
 import { provideClientHydration } from '@angular/platform-browser';
 import { provideRouter } from '@angular/router';
 import { appRoutes } from './app.routes';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 export const appConfig: ApplicationConfig = {
-    providers: [provideClientHydration(), provideRouter(appRoutes)]
+    providers: [provideClientHydration(), provideRouter(appRoutes), provideAnimationsAsync()]
 };
